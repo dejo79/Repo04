aa882f5dca6b75fd3aecb6d9a4a72096d29d77ae
dejo79 <dstefatic@gmail.com>
2021-09-16 16:04:14 +0200

    Added new files

---
+++ ex01/src/index.js
@@ -0,0 +1,22 @@
+function main(numOne, numTwo){
+
+var quotient;
+if(numTwo === 0){
+    quotient = 'Divide by 0! Cannot divide by zero!';
+} else {
+
+var numOne;
+var numTwo;
+var quotient = numOne / numTwo;
+numOne = 5, 25,420, 55;
+numTwo = 3, 5, 10, 0;
+
+}
+
+return quotient;
+
+}
+
+console.log(main(numOne, numTwo));
+
+module.exports = main;
